39ae9fb4b1f57c211afaa3828e604fcbf1ffc4e9
NDICHU <ndichumuriithi@gmail.com>
2022-06-17 01:06:10 +0300

    Completed assignment

--- .history/lib/song_20220617005747.rb
+++ .history/lib/song_20220617005747.rb
@@ -1,12 +1,11 @@
 class Song
-  @@all=[]
+
   attr_accessor :name, :album, :id
 
   def initialize(name:, album:, id: nil)
     @id = id
     @name = name
     @album = album
-    @@all << self
   end
 
   def self.drop_table
@@ -50,33 +49,13 @@ class Song
     song.save
   end
 
-  def self.new_from_db(row)
-    # self.new is equivalent to Song.new
-    self.new(id: row[0], name: row[1], album: row[2])
-  end
-
-  def self.all
+    def self.all
     sql = <<-SQL
       SELECT *
       FROM songs
     SQL
 
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
-  end
-
-  def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT *
-      FROM songs
-      WHERE name = ?
-      LIMIT 1
-    SQL
-
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
+    DB[:conn].execute(sql)
   end
 
 end
